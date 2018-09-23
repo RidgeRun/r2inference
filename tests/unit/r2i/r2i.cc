f4a14c2a27106fb22b30eccde621f3ed4dcce3db
Michael Gruner <michael.gruner@ridgerun.com>
2018-09-22 18:44:39 -0600

    Add boilerplate code for unit tests

---
+++ tests/unit/r2i/r2i.cc
@@ -0,0 +1,31 @@
+/* Copyright (C) 2018 RidgeRun, LLC (http://www.ridgerun.com)
+ * All Rights Reserved.
+ *
+ * The contents of this software are proprietary and confidential to RidgeRun,
+ * LLC.  No part of this program may be photocopied, reproduced or translated
+ * into another programming language without prior written consent of
+ * RidgeRun, LLC.  The user is free to modify the source code after obtaining
+ * a software license from RidgeRun.  All source code changes must be provided
+ * back to RidgeRun without any encumbrance.
+*/
+
+#include <CppUTest/TestHarness.h>
+
+TEST_GROUP (R2Inference)
+{
+  void setup() {
+  }
+
+  void teardown() {
+  }
+};
+
+TEST (R2Inference, FirstTest)
+{
+
+}
+
+TEST (R2Inference, SecondTest)
+{
+
+}
