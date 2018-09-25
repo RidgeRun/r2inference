5d83aa97a8f866da56eaba7dff78191ac5bebd76
Jose Jimenez <jose.jimenez@ridgerun.com>
2018-09-24 18:59:10 -0600

    IFrame: Add interface implementation for IFrame
    This implementation was made based on the UML
    diagram of the project design. Check it for
    more details.

---
+++ r2i/iframe.h
@@ -0,0 +1,28 @@
+/* Copyright (C) 2018 RidgeRun, LLC (http://www.ridgerun.com)
+ * All Rights Reserved.
+ *
+ * The contents of this software are proprietary and confidential to RidgeRun,
+ * LLC.  No part of this program may be photocopied, reproduced or translated
+ * into another programming language without prior written consent of
+ * RidgeRun, LLC.  The user is free to modify the source code after obtaining
+ * a software license from RidgeRun.  All source code changes must be provided
+ * back to RidgeRun without any encumbrance.
+*/
+
+#ifndef R2I_IFRAME_H
+#define R2I_IFRAME_H
+
+/**
+ * R2Inference Namespace
+ */
+namespace r2i {
+  /**
+   * IFrame class implements the interface to abstract data input
+   */
+  class IFrame {
+
+  };
+  
+}
+
+#endif // R2I_IFRAME_H
