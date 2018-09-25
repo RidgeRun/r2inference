e37ba571d66a617ac66a6e0f54a5239aa62e1d77
Jose Jimenez <jose.jimenez@ridgerun.com>
2018-09-24 19:48:25 -0600

    r2i: Add Interface headers to r2i library

--- r2i/r2i.h
+++ r2i/r2i.h
@@ -13,6 +13,10 @@
 #define R2I_R2I_H
 
 #include <r2i/iframeworkfactory.h>
+#include <r2i/iloader.h>
+#include <r2i/iengine.h>
+#include <r2i/iparameters.h>
+#include <r2i/iprediction.h>
 
 namespace r2i
 {
