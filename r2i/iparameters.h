12d353e8d0b96020b25bd9b88b12228fb6b90042
Jose Jimenez <jose.jimenez@ridgerun.com>
2018-09-26 16:55:38 -0600

    Add interface implementation for IParameters
    This implementation was made based on the UML
    diagram of the project design. Check it for
    more details.

---
+++ r2i/iparameters.h
@@ -0,0 +1,86 @@
+/* Copyright (C) 2018 RidgeRun, LLC (http://www.ridgerun.com)
+ * All Rights Reserved.
+ *
+ * The contents of this software are proprietary and confidential to RidgeRun,
+ * LLC.  No part of this program may be photocopied, reproduced or translated
+ * into another programming language without prior written consent of
+ * RidgeRun, LLC.  The user is free to modify the source code after obtaining
+ * a software license from RidgeRun.  All source code changes must be provided
+ * back to RidgeRun without any encumbrance.
+*/
+
+#ifndef R2I_IPARAMETERS_H
+#define R2I_IPARAMETERS_H
+
+#include <r2i/iengine.h>
+#include <r2i/imodel.h>
+#include <r2i/runtime_error.h>
+
+#include <memory>
+#include <string>
+
+/**
+ * R2Inference Namespace
+ */
+namespace r2i
+{
+/**
+ *  Implements the interface to abstract IFrameworkFactory parameters. This
+ *  interface can be use to set and get parameters available in a framework
+ *  factory.
+ */
+
+class IParameters
+{
+
+public:
+  /**
+   * \brief Sets an IEngine and IModel as targets for the parameters accessors.
+   * \param in_engine IEngine available for a IFrameworkFactory.
+   * \param in_model IModel compatible with the IFrameworkFactory.
+   * \param error [out] RuntimeError with a description of an error.
+   */
+  virtual void Configure (std::unique_ptr<r2i::IEngine> in_engine,
+    std::unique_ptr<r2i::IModel> in_model,
+    RuntimeError &error ) = 0;
+
+  /**
+   * \brief Queries an integer parameter.
+   * \param in_parameter Name of the parameter to get a value
+   * \param value [out] Return value of the parameter to query
+   * \param error [out] RuntimeError with a description of an error.
+   */
+  virtual void Get (const std::string in_parameter, int &value,
+    r2i::RuntimeError &error ) = 0;
+
+  /**
+   * \brief Queries a string parameter.
+   * \param in_parameter Name of the parameter to get a value
+   * \param value [out] Return value of the parameter to query
+   * \param error [out] RuntimeError with a description of an error.
+   */
+  virtual void Get (const std::string in_parameter, const std::string &value,
+    r2i::RuntimeError &error ) = 0;
+
+  /**
+   * \brief Sets a string parameter.
+   * \param in_parameter  Name of the parameter to set a value
+   * \param in_value New value to set for in_parameter
+   * \param error [out] RuntimeError with a description of an error.
+   */
+  virtual void Set (const std::string in_parameter, const std::string &in_value,
+    RuntimeError &error ) = 0;
+
+  /**
+   * \brief Sets an integer parameter.
+   * \param in_parameter Name of the parameter to set a value
+   * \param in_value New value to set for in_parameter
+   * \param error [out] RuntimeError with a description of an error.
+   */
+  virtual void Set (const std::string in_parameter, int in_value,
+    RuntimeError &error ) = 0;
+};
+
+}
+
+#endif // R2I_IPARAMETERS_H
