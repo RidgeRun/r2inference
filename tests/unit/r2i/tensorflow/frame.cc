65414b1d1fdb9a90b9530d9b46f312a0b169db82
Emmanuel Madrigal <emmanuel.madrigal@ridgerun.com>
2019-02-01 08:55:47 -0600

    tensorflow: Add memory leak detection to frame tests

--- tests/unit/r2i/tensorflow/frame.cc
+++ tests/unit/r2i/tensorflow/frame.cc
@@ -18,6 +18,8 @@
 #include <tensorflow/c/c_api.h>
 
 #include <CppUTest/CommandLineTestRunner.h>
+#include <CppUTest/MemoryLeakDetectorNewMacros.h>
+#include <CppUTest/MemoryLeakDetectorMallocMacros.h>
 #include <CppUTest/TestHarness.h>
 
 #define FRAME_WIDTH  2
@@ -28,10 +30,11 @@ bool invalid_width = false;
 bool invalid_height = false;
 bool invalid_channels = false;
 bool new_tensor_error = false;
+void *dummyprt = nullptr;
 
 int TF_GraphGetTensorNumDims(TF_Graph *graph, TF_Output output,
                              TF_Status *status) {
-  return 1;
+  return 4;
 }
 void TF_GraphGetTensorShape(TF_Graph *graph, TF_Output output, int64_t *dims,
                             int num_dims, TF_Status *status) {
@@ -63,24 +66,19 @@ void TF_GraphGetTensorShape(TF_Graph *graph, TF_Output output, int64_t *dims,
 TF_DataType TF_OperationOutputType(TF_Output oper_out) {
   return TF_FLOAT;
 }
-TF_Graph *TF_NewGraph() { return (TF_Graph *) new int; }
+TF_Graph *TF_NewGraph() { return (TF_Graph *) dummyprt; }
 void TF_DeleteGraph(TF_Graph *g) {
-  if (g == nullptr) return;
-  delete (int *) g;
 }
-// TF_Tensor* TF_NewTensor(TF_DataType dtype, const int64_t* dims, int num_dims, void* data, size_t len, void (*deallocator)(void* data, size_t len, void* arg), void* deallocator_arg) { return (TF_Tensor *) new int; }
 TF_Tensor *TF_NewTensor(TF_DataType dtype, const int64_t *dims, int num_dims,
                         void *data, size_t len, void (*deallocator)(void *data, size_t len, void *arg),
                         void *deallocator_arg) {
   if (new_tensor_error) {
     return (TF_Tensor *) nullptr;
   } else {
-    return (TF_Tensor *) new int;
+    return (TF_Tensor *) dummyprt;
   }
 }
 void TF_DeleteTensor(TF_Tensor *g) {
-  if (g == nullptr) return;
-  delete (int *) g;
 }
 
 TEST_GROUP (TensorflowFrame) {
@@ -91,10 +89,13 @@ TEST_GROUP (TensorflowFrame) {
   r2i::ImageFormat format;
 
   void setup () {
+    frame = r2i::tensorflow::Frame();
     format = r2i::ImageFormat(r2i::ImageFormat::Id::RGB);
+    dummyprt = malloc(1);
   }
 
   void teardown () {
+    free(dummyprt);
   }
 };
 
@@ -194,6 +195,9 @@ TEST (TensorflowFrame, FrameGetTensorNullGraph) {
                             data; /* Used to avoid passing a nullptr */
   std::shared_ptr<TF_Tensor> tensor(nullptr);
 
+  error = frame.Configure(data, width, height, format.GetId());
+  LONGS_EQUAL (r2i::RuntimeError::Code::EOK, error.GetCode());
+
   tensor = frame.GetTensor(nullptr, operation, error);
   LONGS_EQUAL (r2i::RuntimeError::Code::INVALID_FRAMEWORK_PARAMETER,
                error.GetCode());
@@ -203,6 +207,9 @@ TEST (TensorflowFrame, FrameGetTensorNullOperation) {
   r2i::RuntimeError error;
   std::shared_ptr<TF_Graph> pgraph(TF_NewGraph(), TF_DeleteGraph);
   std::shared_ptr<TF_Tensor> tensor(nullptr);
+
+  error = frame.Configure(data, width, height, format.GetId());
+  LONGS_EQUAL (r2i::RuntimeError::Code::EOK, error.GetCode());
 
   tensor = frame.GetTensor(pgraph, nullptr, error);
   LONGS_EQUAL (r2i::RuntimeError::Code::INVALID_FRAMEWORK_PARAMETER,
@@ -217,6 +224,9 @@ TEST (TensorflowFrame, FrameGetTensorMultipleBatches) {
   std::shared_ptr<TF_Tensor> tensor(nullptr);
 
   multiple_batches = true;
+
+  error = frame.Configure(data, width, height, format.GetId());
+  LONGS_EQUAL (r2i::RuntimeError::Code::EOK, error.GetCode());
 
   tensor = frame.GetTensor(pgraph, operation, error);
   LONGS_EQUAL (r2i::RuntimeError::Code::INVALID_FRAMEWORK_PARAMETER,
@@ -234,6 +244,9 @@ TEST (TensorflowFrame, FrameGetTensorUnsupportedWidth) {
 
   invalid_width = true;
 
+  error = frame.Configure(data, width, height, format.GetId());
+  LONGS_EQUAL (r2i::RuntimeError::Code::EOK, error.GetCode());
+
   tensor = frame.GetTensor(pgraph, operation, error);
   LONGS_EQUAL (r2i::RuntimeError::Code::INVALID_FRAMEWORK_PARAMETER,
                error.GetCode());
@@ -249,6 +262,9 @@ TEST (TensorflowFrame, FrameGetTensorUnsupportedHeight) {
   std::shared_ptr<TF_Tensor> tensor(nullptr);
 
   invalid_height = true;
+
+  error = frame.Configure(data, width, height, format.GetId());
+  LONGS_EQUAL (r2i::RuntimeError::Code::EOK, error.GetCode());
 
   tensor = frame.GetTensor(pgraph, operation, error);
   LONGS_EQUAL (r2i::RuntimeError::Code::INVALID_FRAMEWORK_PARAMETER,
@@ -266,6 +282,9 @@ TEST (TensorflowFrame, FrameGetTensorIncorrectChannels) {
 
   invalid_channels = true;
 
+  error = frame.Configure(data, width, height, format.GetId());
+  LONGS_EQUAL (r2i::RuntimeError::Code::EOK, error.GetCode());
+
   tensor = frame.GetTensor(pgraph, operation, error);
   LONGS_EQUAL (r2i::RuntimeError::Code::INVALID_FRAMEWORK_PARAMETER,
                error.GetCode());
@@ -282,15 +301,16 @@ TEST (TensorflowFrame, FrameGetTensorNewTensorError) {
 
   new_tensor_error = true;
 
+  error = frame.Configure(data, width, height, format.GetId());
+  LONGS_EQUAL (r2i::RuntimeError::Code::EOK, error.GetCode());
+
   tensor = frame.GetTensor(pgraph, operation, error);
-  LONGS_EQUAL (r2i::RuntimeError::Code::INVALID_FRAMEWORK_PARAMETER,
+  LONGS_EQUAL (r2i::RuntimeError::Code::FRAMEWORK_ERROR,
                error.GetCode());
 
   new_tensor_error = false;
 }
 
 int main (int ac, char **av) {
-  MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
-
   return CommandLineTestRunner::RunAllTests (ac, av);
 }
