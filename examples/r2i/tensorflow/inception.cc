be996a5e3dd7b0da461b85ac1abf7f9e677e025b
GFallasRR <greivin.fallas@ridgerun.com>
2019-06-12 09:37:58 -0600

    Update TensorFlow Inception example

--- examples/r2i/tensorflow/inception.cc
+++ examples/r2i/tensorflow/inception.cc
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018 RidgeRun, LLC (http://www.ridgerun.com)
+/* Copyright (C) 2019 RidgeRun, LLC (http://www.ridgerun.com)
  * All Rights Reserved.
  *
  * The contents of this software are proprietary and confidential to RidgeRun,
@@ -13,7 +13,6 @@
 #include <iostream>
 #include <memory>
 #include <string>
-
 #include <r2i/r2i.h>
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -22,14 +21,11 @@
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize.h"
 
-#define GOOGLENET_DIM 299
-
 void PrintTopPrediction (std::shared_ptr<r2i::IPrediction> prediction) {
   r2i::RuntimeError error;
   int index = 0;
   double max = -1;
-
-  int num_labels = prediction->GetResultSize() / sizeof(float) ;
+  int num_labels = prediction->GetResultSize() / sizeof(float);
 
   for (int i = 0; i < num_labels; ++i) {
     double current = prediction->At(i, error);
@@ -39,21 +35,28 @@ void PrintTopPrediction (std::shared_ptr<r2i::IPrediction> prediction) {
     }
   }
 
-  std::cout << "Highest probability is label " << index <<
-            " (" << max << ")" << std::endl;
+  std::cout << "Highest probability is label " 
+            << index << " (" << max << ")" << std::endl;
 }
 
 void PrintUsage() {
-  std::cerr << "Usage: example -i [JPG input_image] -m [GoogLeNet Model]" <<
-            std::endl;
+  std::cerr << "Required arguments: "
+            << "-i [JPG input_image] "
+            << "-m [Inception TensorFlow Model] "
+            << "-s [Model Input Size] "
+            << "-I [Input Node] "
+            << "-O [Output Node] \n" 
+            << " Example: "
+            << " ./inception -i cat.jpg -m graph_inceptionv2_tensorflow.pb "
+            << "-s 224 -I input -O Softmax"
+            << std::endl;
 }
 
 std::unique_ptr<float[]> PreProcessImage (const unsigned char *input,
-    int width, int height, int reqwidth,
-    int reqheight) {
+    int width, int height, int reqwidth, int reqheight) {
+
   const int channels = 3;
   const int scaled_size = channels * reqwidth * reqheight;
-
   std::unique_ptr<unsigned char[]> scaled (new unsigned char[scaled_size]);
   std::unique_ptr<float[]> adjusted (new float[scaled_size]);
 
@@ -61,7 +64,7 @@ std::unique_ptr<float[]> PreProcessImage (const unsigned char *input,
                      reqheight, 0, channels);
 
   for (int i = 0; i < scaled_size; i += channels) {
-    // BGR = (RGB - Mean)*StdDev
+    /* RGB = (RGB - Mean)*StdDev */
     adjusted[i + 0] = (static_cast<float>(scaled[i + 0]) - 128) / 128.0;
     adjusted[i + 1] = (static_cast<float>(scaled[i + 1]) - 128) / 128.0;
     adjusted[i + 2] = (static_cast<float>(scaled[i + 2]) - 128) / 128.0;
@@ -88,10 +91,11 @@ std::unique_ptr<float[]> LoadImage(const std::string &path, int reqwidth,
 }
 
 bool ParseArgs (int &argc, char *argv[], std::string &image_path,
-                std::string &model_path, int &index) {
-  int option = 0;
+                std::string &model_path, int &index, int &size,
+                std::string &in_node, std::string &out_node) {
 
-  while ((option = getopt(argc, argv, "i:m:p:")) != -1) {
+  int option = 0;
+  while ((option = getopt(argc, argv, "i:m:p:s:I:O:")) != -1) {
     switch (option) {
       case 'i' :
         image_path = optarg;
@@ -102,22 +106,34 @@ bool ParseArgs (int &argc, char *argv[], std::string &image_path,
       case 'p' :
         index  = std::stoi (optarg);
         break;
+      case 's' :
+        size = std::stoi (optarg);
+        break;
+      case 'I' :
+        in_node = optarg;
+        break;
+      case 'O' :
+        out_node = optarg;
+        break;
       default:
         return false;
     }
   }
-
   return true;
 }
 
-
 int main (int argc, char *argv[]) {
+
   r2i::RuntimeError error;
   std::string model_path;
   std::string image_path;
+  std::string in_node;
+  std::string out_node;
   int Index = 0;
+  int size = 0;
 
-  if (false == ParseArgs (argc, argv, image_path, model_path, Index)) {
+  if (false == ParseArgs (argc, argv, image_path, model_path, Index,
+                          size, in_node, out_node)) {
     PrintUsage ();
     exit (EXIT_FAILURE);
   }
@@ -131,7 +147,7 @@ int main (int argc, char *argv[]) {
                    r2i::FrameworkCode::TENSORFLOW,
                    error);
 
-  std::cout << "Loading Model: " << model_path << "..." << std::endl;
+  std::cout << "Loading Model: " << model_path << std::endl;
   auto loader = factory->MakeLoader (error);
   std::shared_ptr<r2i::IModel> model = loader->Load (model_path, error);
   if (error.IsError ()) {
@@ -139,27 +155,27 @@ int main (int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  std::cout << "Setting model to engine..." << std::endl;
+  std::cout << "Setting model to engine" << std::endl;
   std::shared_ptr<r2i::IEngine> engine = factory->MakeEngine (error);
   error = engine->SetModel (model);
 
-  std::cout << "Configuring input and output layers..." << std::endl;
+  std::cout << "Configuring input and output layers" << std::endl;
   auto params = factory->MakeParameters (error);
   error = params->Configure(engine, model);
-  params->Set ("input-layer", "input");
-  params->Set ("output-layer", "InceptionV4/Logits/Predictions");
+  params->Set ("input-layer", in_node);
+  params->Set ("output-layer", out_node);
 
-  std::cout << "Loading image: " << image_path << "..." << std::endl;
-  std::unique_ptr<float[]> image_data = LoadImage (image_path, GOOGLENET_DIM,
-                                        GOOGLENET_DIM);
+  std::cout << "Loading image: " << image_path << std::endl;
+  std::unique_ptr<float[]> image_data = LoadImage (image_path, size,
+                                                   size);
 
-  std::cout << "Configuring frame..." << std::endl;
+  std::cout << "Configuring frame" << std::endl;
   std::shared_ptr<r2i::IFrame> frame = factory->MakeFrame (error);
 
-  error = frame->Configure (image_data.get(), GOOGLENET_DIM, GOOGLENET_DIM,
+  error = frame->Configure (image_data.get(), size, size,
                             r2i::ImageFormat::Id::RGB);
 
-  std::cout << "Starting engine..." << std::endl;
+  std::cout << "Starting engine" << std::endl;
   error = engine->Start ();
   if (error.IsError ()) {
     std::cerr << "Engine start error: " << error << std::endl;
@@ -175,7 +191,7 @@ int main (int argc, char *argv[]) {
 
   PrintTopPrediction (prediction);
 
-  std::cout << "Stopping engine..." << std::endl;
+  std::cout << "Stopping engine" << std::endl;
   error = engine->Stop ();
   if (error.IsError ()) {
     std::cerr << "Engine stop error: " << error << std::endl;
@@ -184,3 +200,4 @@ int main (int argc, char *argv[]) {
 
   return EXIT_SUCCESS;
 }
+
