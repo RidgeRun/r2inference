33db5b50e29af33248f9ff895a004a3a054fbc49
jafet-chaves <jafet.chaves@ridgerun.com>
2020-06-07 17:43:26 -0600

    Fix loader module tests
    *Expand tests according to new loader implementation
    *Add mocks for external ONNXRT objects. This is a workaround
    for current seg fault issue while using the true implementations.
    The mocks simulate std::exception derived exception which allow to
    test the loader implementation when catching those exceptions.
    Sample applications do not cause seg faults using the current loader
    implementation.

--- tests/unit/r2i/onnxrt/loader.cc
+++ tests/unit/r2i/onnxrt/loader.cc
@@ -9,87 +9,142 @@
  * back to RidgeRun without any encumbrance.
 */
 
-#include <r2i/r2i.h>
 #include <r2i/onnxrt/loader.h>
+#include <r2i/r2i.h>
+
+#include <onnxruntime/core/common/exceptions.h>
+#include <onnxruntime/core/session/onnxruntime_cxx_api.h>
+
+#include <iostream>
 
 #include <CppUTest/CommandLineTestRunner.h>
+#include <CppUTest/MemoryLeakDetectorMallocMacros.h>
+#include <CppUTest/MemoryLeakDetectorNewMacros.h>
 #include <CppUTest/TestHarness.h>
 
 static bool incompatible_model = false;
+static bool env_allocation_fail = false;
+static bool session_options_allocation_fail = false;
+static bool session_allocation_fail = false;
 
-r2i::onnxrt::Model::Model () {
-}
+// To simulate exceptions thrown by onnxruntime API. Exceptions in this
+// API are derived from std::exception.
+class OnnxrtExcep : public std::exception {
+  virtual const char *what() const throw() { return "ONNXRT exception thrown"; }
+} onnxrtexcep;
 
-r2i::RuntimeError r2i::onnxrt::Model::Start (const std::string &name) {
+r2i::onnxrt::Model::Model() {}
+
+r2i::RuntimeError r2i::onnxrt::Model::Set(
+  std::shared_ptr<Ort::Session> onnxrt_session) {
   r2i::RuntimeError error;
 
   if (incompatible_model) {
-    error.Set (r2i::RuntimeError::Code::INCOMPATIBLE_MODEL, "Incompatible model");
+    error.Set(r2i::RuntimeError::Code::INCOMPATIBLE_MODEL,
+              "Incompatible model");
   }
 
   return error;
 }
 
-TEST_GROUP (OnnxrtLoader) {
+// FIXME: We are mocking onnxrt constructors. We simulate exceptions
+// thrown constructing these objects.
+void r2i::onnxrt::Loader::CreateSession(
+  std::shared_ptr<Ort::Env> env, const std::string &name,
+  std::shared_ptr<Ort::SessionOptions> options) {
+  if (incompatible_model || session_allocation_fail) {
+    throw onnxrtexcep;
+  }
+}
+
+void r2i::onnxrt::Loader::CreateEnv(OrtLoggingLevel log_level,
+                                    const std::string &log_id) {
+  if (env_allocation_fail) {
+    throw onnxrtexcep;
+  }
+}
+
+void r2i::onnxrt::Loader::CreateSessionOptions() {
+  if (session_options_allocation_fail) {
+    throw onnxrtexcep;
+  }
+}
+
+TEST_GROUP(OnnxrtLoader) {
   r2i::RuntimeError error;
   r2i::onnxrt::Loader loader;
 
-  void setup () {
+  void setup() {
     error.Clean();
     loader = r2i::onnxrt::Loader();
     incompatible_model = false;
+    env_allocation_fail = false;
+    session_options_allocation_fail = false;
+    session_allocation_fail = false;
   }
 };
 
-TEST (OnnxrtLoader, WrongApiUsage) {
+TEST(OnnxrtLoader, WrongApiUsage) {
   std::string empty_string = "";
 
   /* Attempt to load this file as a valid model */
   auto model = loader.Load(empty_string, error);
 
-  CHECK_TEXT (error.IsError(), error.GetDescription().c_str());
-  LONGS_EQUAL (r2i::RuntimeError::Code::WRONG_API_USAGE, error.GetCode ());
+  CHECK_TEXT(error.IsError(), error.GetDescription().c_str());
+  LONGS_EQUAL(r2i::RuntimeError::Code::WRONG_API_USAGE, error.GetCode());
 }
 
-TEST (OnnxrtLoader, UnableToOpenFile) {
-  std::string non_existent_file = "*\"?";
+TEST(OnnxrtLoader, UnableToOpenFile) {
+  std::string non_existent_file = "resources/squeezenet_typo.onnx";
 
   /* Attempt to load this file as a valid model */
   auto model = loader.Load(non_existent_file, error);
 
-  CHECK_TEXT (error.IsError(), error.GetDescription().c_str());
-  LONGS_EQUAL (r2i::RuntimeError::Code::FILE_ERROR, error.GetCode ());
+  CHECK_TEXT(error.IsError(), error.GetDescription().c_str());
+  LONGS_EQUAL(r2i::RuntimeError::Code::FILE_ERROR, error.GetCode());
 }
 
-TEST (OnnxrtLoader, UnableToReadFile) {
-  /* This will work as long as make check is run as a regular user */
-  std::string empty_string = "/root";
+TEST(OnnxrtLoader, EnvAllocationFail) {
+  env_allocation_fail = true;
+  std::string model_path = "resources/squeezenet.onnx";
+  auto model = loader.Load(model_path, error);
 
-  /* Attempt to load an unreadable file */
-  auto model = loader.Load(empty_string, error);
-
-  CHECK_TEXT (error.IsError(), error.GetDescription().c_str());
-  LONGS_EQUAL (r2i::RuntimeError::Code::FILE_ERROR, error.GetCode ());
+  LONGS_EQUAL(r2i::RuntimeError::Code::FRAMEWORK_ERROR, error.GetCode());
 }
 
-TEST (OnnxrtLoader, LoadInvalidFile) {
-  /* Setup */
+TEST(OnnxrtLoader, SessionOptionsAllocationFail) {
+  session_options_allocation_fail = true;
+  std::string model_path = "resources/squeezenet.onnx";
+  auto model = loader.Load(model_path, error);
+
+  LONGS_EQUAL(r2i::RuntimeError::Code::FRAMEWORK_ERROR, error.GetCode());
+}
+
+TEST(OnnxrtLoader, SessionAllocationFail) {
+  session_allocation_fail = true;
+  std::string model_path = "resources/squeezenet.onnx";
+  auto model = loader.Load(model_path, error);
+
+  LONGS_EQUAL(r2i::RuntimeError::Code::FRAMEWORK_ERROR, error.GetCode());
+}
+
+TEST(OnnxrtLoader, LoadInvalidFile) {
   incompatible_model = true;
+  std::string model_path = "resources/invalid.onnx";
+  auto model = loader.Load(model_path, error);
 
-  /* Attempt to load this file as a valid model */
-  auto model = loader.Load(__FILE__, error);
-
-  CHECK_TEXT (error.IsError(), error.GetDescription().c_str());
-  LONGS_EQUAL (r2i::RuntimeError::Code::INCOMPATIBLE_MODEL, error.GetCode ());
+  LONGS_EQUAL(r2i::RuntimeError::Code::FRAMEWORK_ERROR, error.GetCode());
 }
 
-TEST (OnnxrtLoader, LoadSuccess) {
-  /* FIXME this isn't actually sending a correct model */
-  auto model = loader.Load(__FILE__, error);
+TEST(OnnxrtLoader, LoadValidFile) {
+  incompatible_model = false;
+  std::string model_path = "resources/squeezenet.onnx";
+  auto model = loader.Load(model_path, error);
 
-  LONGS_EQUAL (r2i::RuntimeError::Code::EOK, error.GetCode ());
+  LONGS_EQUAL(r2i::RuntimeError::Code::EOK, error.GetCode());
 }
 
-int main (int ac, char **av) {
-  return CommandLineTestRunner::RunAllTests (ac, av);
+int main(int ac, char **av) {
+  MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
+  return CommandLineTestRunner::RunAllTests(ac, av);
 }
