4cca4e0edaceb026bb8dc3e18afc009a7a12c423
Luis Alonso Murillo Rojas <luis.murillo@ridgerun.com>
2020-09-22 15:37:36 -0600

    Add inference output classes

---
+++ r2i/classification.cc
@@ -0,0 +1,51 @@
+/* Copyright (C) 2020 RidgeRun, LLC (http://www.ridgerun.com)
+ * All Rights Reserved.
+ *
+ * The contents of this software are proprietary and confidential to RidgeRun,
+ * LLC.  No part of this program may be photocopied, reproduced or translated
+ * into another programming language without prior written consent of
+ * RidgeRun, LLC.  The user is free to modify the source code after obtaining
+ * a software license from RidgeRun.  All source code changes must be provided
+ * back to RidgeRun without any encumbrance.
+*/
+
+#include "classification.h"
+
+namespace r2i {
+
+Classification::Classification(InferenceOutputType output_type) {
+  this->type = output_type;
+}
+
+Classification::~Classification() {
+  this->labels.clear();
+}
+
+RuntimeError Classification::SetLabels(std::vector< ClassificationBin >
+                                       labels) {
+  this->labels = labels;
+  return RuntimeError();
+}
+
+std::vector< ClassificationBin > Classification::GetLabels() {
+  return this->labels;
+}
+
+ClassificationBin Classification::GetRank1() {
+  ClassificationBin max_scored_class;
+
+  max_scored_class = std::make_tuple(0, 0.0);
+
+  for (size_t index = 0; index < this->labels.size(); index++) {
+    double score = std::get<CLASS_SCORE_INDEX>(this->labels[index]);
+    double current_max_score = std::get<CLASS_SCORE_INDEX>(max_scored_class);
+
+    if (score > current_max_score) {
+      max_scored_class = this->labels[index];
+    }
+  }
+
+  return max_scored_class;
+}
+
+}
