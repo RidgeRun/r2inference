f49062ed48fcfe09a5fae4c827848a6c5c3e975e
jafet-chaves <jafet.chaves@ridgerun.com>
2020-09-21 21:57:51 -0600

    Extend tests for Normalize preprocessing module

--- tests/unit/r2i/preprocessing/normalize.cc
+++ tests/unit/r2i/preprocessing/normalize.cc
@@ -20,14 +20,17 @@
 
 #include <r2i/r2i.h>
 
-#define FRAME_WIDTH 224
-#define FRAME_HEIGHT 224
+#define FRAME_WIDTH 2
+#define FRAME_HEIGHT 2
+#define MEAN 0
+#define STD_DEV 1
 #define UNSUPPORTED_FRAME_WIDTH -1
 #define UNSUPPORTED_FRAME_HEIGHT -1
 #define CHANNELS  3
 
-r2i::ImageFormat image_format_rgb (r2i::ImageFormat::Id::RGB);
-std::vector<float> dummy_frame_data(CHANNELS *FRAME_WIDTH *FRAME_HEIGHT);
+static const float reference_matrix[CHANNELS * FRAME_WIDTH * FRAME_HEIGHT] = {
+  1.0, 2.0, 3.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 10.0
+};
 
 namespace mock {
 class Frame : public r2i::IFrame {
@@ -36,27 +39,47 @@ class Frame : public r2i::IFrame {
   r2i::RuntimeError Configure (void *in_data, int width,
                                int height, r2i::ImageFormat::Id format) {
 
-    for (unsigned int i = 0; i < dummy_frame_data.size() - 1; i++) {
-      dummy_frame_data.at(i) = i;
+    r2i::RuntimeError error;
+    r2i::ImageFormat imageformat (format);
+
+    if (nullptr == in_data) {
+      error.Set (r2i::RuntimeError::Code::NULL_PARAMETER,
+                 "Received a NULL data pointer");
+      return error;
+    }
+    if (width <= 0) {
+      error.Set (r2i::RuntimeError::Code::WRONG_API_USAGE,
+                 "Received an invalid image width");
+      return error;
+    }
+    if (height <= 0) {
+      error.Set (r2i::RuntimeError::Code::WRONG_API_USAGE,
+                 "Received an invalid image height");
+      return error;
     }
 
-    return r2i::RuntimeError();
+    this->frame_data = static_cast<unsigned char *>(in_data);
+    this->frame_width = width;
+    this->frame_height = height;
+    this->frame_format = imageformat;
+
+    return error;
   }
 
   void *GetData () {
-    return dummy_frame_data.data();
+    return this->frame_data;
   }
 
   int GetWidth () {
-    return FRAME_WIDTH;
+    return this->frame_width;
   }
 
   int GetHeight () {
-    return FRAME_HEIGHT;
+    return this->frame_height;
   }
 
   r2i::ImageFormat GetFormat () {
-    return image_format_rgb;
+    return this->frame_format;
   }
 
   r2i::DataType GetDataType () {
@@ -64,31 +87,66 @@ class Frame : public r2i::IFrame {
   }
 
  private:
-  float *frame_data;
+  unsigned char *frame_data = nullptr;
   int frame_width;
   int frame_height;
   r2i::ImageFormat frame_format;
+};
+
+class NormalizeMock : public r2i::Normalize {
+
+ public:
+  NormalizeMock () {
+    this->dimensions.push_back(std::tuple<int, int>(FRAME_WIDTH, FRAME_HEIGHT));
+  }
+
+ private:
+  r2i::RuntimeError SetNormalizationParameters () {
+    this->mean = MEAN;
+    this->std_dev = STD_DEV;
+    return r2i::RuntimeError();
+  }
+
 };
 }
 
 TEST_GROUP(Normalize) {
   r2i::RuntimeError error;
   std::shared_ptr<r2i::IPreprocessing> preprocessing =
-    std::make_shared<r2i::Normalize>();
+    std::make_shared<mock::NormalizeMock>();
+  std::shared_ptr<unsigned char> dummy_frame_data;
   std::shared_ptr<mock::Frame> in_frame = std::make_shared<mock::Frame>();
   std::shared_ptr<mock::Frame> out_frame = std::make_shared<mock::Frame>();
 
   void setup() {
     error.Clean();
-    error = in_frame->Configure(dummy_frame_data.data(), FRAME_WIDTH, FRAME_HEIGHT,
-                                image_format_rgb.GetId());
   }
 };
 
 TEST(Normalize, ApplySuccess) {
+  unsigned char value = 0;
+  dummy_frame_data = std::shared_ptr<unsigned char>(new unsigned char[12],
+                     std::default_delete<const unsigned char[]>());
+
+  unsigned char *in_data = dummy_frame_data.get();
+
+  for (unsigned int i = 0; i < 12; i++) {
+    in_data[i] = value;
+    value++;
+  }
+
+  error = in_frame->Configure(in_data, FRAME_WIDTH, FRAME_HEIGHT,
+                              r2i::ImageFormat::Id::RGB);
+
   error = preprocessing->Apply(in_frame, out_frame, FRAME_WIDTH, FRAME_HEIGHT,
                                r2i::ImageFormat::Id::RGB);
   LONGS_EQUAL(r2i::RuntimeError::Code::EOK, error.GetCode());
+
+  /* Check output values are the expected ones */
+  for (unsigned int i = 0; i < 12; i++) {
+    LONGS_EQUAL(static_cast<float *>(out_frame->GetData())[i], reference_matrix[i]);
+  }
+
 }
 
 TEST(Normalize, UnsupportedHeight) {
@@ -110,6 +168,13 @@ TEST(Normalize, UnsupportedFormatId) {
                                UNSUPPORTED_FRAME_WIDTH,
                                r2i::ImageFormat::Id::BGR);
   LONGS_EQUAL(r2i::RuntimeError::Code::MODULE_ERROR, error.GetCode());
+}
+
+TEST(Normalize, NullFrameData) {
+  error = preprocessing->Apply(in_frame, out_frame, FRAME_WIDTH,
+                               FRAME_HEIGHT,
+                               r2i::ImageFormat::Id::RGB);
+  LONGS_EQUAL(r2i::RuntimeError::Code::NULL_PARAMETER, error.GetCode());
 }
 
 int main(int ac, char **av) {
