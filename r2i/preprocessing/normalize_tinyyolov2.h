3b94f625eaaeb94d63b0564f13cde24c0517515a
jafet-chaves <jafet.chaves@ridgerun.com>
2020-09-21 23:06:20 -0600

    Add preprocessing modules for tinyyolo models

---
+++ r2i/preprocessing/normalize_tinyyolov2.h
@@ -0,0 +1,28 @@
+/* Copyright (C) 2020 RidgeRun, LLC (http://www.ridgerun.com)
+ * All Rights Reserved.
+ *
+ * The contents of this software are proprietary and confidential to RidgeRun,
+ * LLC.  No part of this program may be photocopied, reproduced or translated
+ * into another programming language without prior written consent of
+ * RidgeRun, LLC.  The user is free to modify the source code after obtaining
+ * a software license from RidgeRun.  All source code changes must be provided
+ * back to RidgeRun without any encumbrance.
+*/
+
+#ifndef R2I_NORMALIZE_TINYYOLOV2_PREPROCESSING_H
+#define R2I_NORMALIZE_TINYYOLOV2_PREPROCESSING_H
+
+#include <r2i/preprocessing/normalize.h>
+
+namespace r2i {
+
+class NormalizeTinyyoloV2: public r2i::Normalize {
+ public:
+  NormalizeTinyyoloV2 ();
+ private:
+  r2i::RuntimeError SetNormalizationParameters () override;
+};
+
+}  // namespace r2i
+
+#endif  // R2I_NORMALIZE_TINYYOLOV2_PREPROCESSING_H
---
+++ r2i/preprocessing/normalize_tinyyolov3.cc
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 RidgeRun, LLC (http://www.ridgerun.com)
+ * All Rights Reserved.
+ *
+ * The contents of this software are proprietary and confidential to RidgeRun,
+ * LLC.  No part of this program may be photocopied, reproduced or translated
+ * into another programming language without prior written consent of
+ * RidgeRun, LLC.  The user is free to modify the source code after obtaining
+ * a software license from RidgeRun.  All source code changes must be provided
+ * back to RidgeRun without any encumbrance.
+ */
+
+#include <memory>
+#include <vector>
+
+#include <r2i/preprocessing/normalize_tinyyolov3.h>
+
+/* Architecture specific required dimensions TinyyoloV3 */
+#define REQ_WIDTH_416 416
+#define REQ_HEIGTH_416 416
+
+/* Constants for preprocessing */
+#define MEAN 0
+#define STD_DEV 1
+
+namespace r2i {
+
+NormalizeTinyyoloV3::NormalizeTinyyoloV3 () : r2i::Normalize() {
+  /* Set supported dimensions for TinyyoloV3 architecture */
+  this->dimensions.push_back(std::tuple<int, int>(REQ_WIDTH_416, REQ_HEIGTH_416));
+}
+
+r2i::RuntimeError NormalizeTinyyoloV3::SetNormalizationParameters () {
+  this->mean_red = MEAN;
+  this->mean_green = MEAN;
+  this->mean_blue = MEAN;
+  this->std_dev_red = STD_DEV;
+  this->std_dev_green = STD_DEV;
+  this->std_dev_blue = STD_DEV;
+  return r2i::RuntimeError();
+}
+
+}  // namespace r2i
+
+r2i::IPreprocessing *
+FactoryMakePreprocessing () {
+  return new r2i::NormalizeTinyyoloV3 ();
+}
