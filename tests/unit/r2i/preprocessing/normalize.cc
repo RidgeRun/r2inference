eb1e60d15ff9e7bb4dab7fb12523b772ab24113c
jafet-chaves <jafet.chaves@ridgerun.com>
2020-09-29 16:59:04 -0600

    Create individual tests for each preprocessing module

--- tests/unit/r2i/preprocessing/normalize.cc
+++ tests/unit/r2i/preprocessing/normalize.cc
@@ -10,13 +10,6 @@
 */
 
 #include "r2i/preprocessing/normalize.h"
-#include "r2i/preprocessing/normalize_facenetv1.h"
-#include "r2i/preprocessing/normalize_inceptionv1.h"
-#include "r2i/preprocessing/normalize_inceptionv3.h"
-#include "r2i/preprocessing/normalize_resnet50v1.h"
-#include "r2i/preprocessing/normalize_tinyyolov2.h"
-#include "r2i/preprocessing/normalize_tinyyolov3.h"
-
 #include <memory>
 
 #include <CppUTest/CommandLineTestRunner.h>
@@ -38,26 +31,6 @@
 
 static const float reference_matrix[FRAME_SIZE] = {
   0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0
-};
-
-static const float reference_matrix_facenetv1[FRAME_SIZE] = {
-  -1.593255, -1.303572, -1.013890, -0.724207, -0.434524, -0.144841,
-    0.144841, 0.434524, 0.724207, 1.013890, 1.303572, 1.593255
-  };
-
-static const float reference_matrix_inception[FRAME_SIZE] = {
-  -1.000000, -0.992188, -0.984375, -0.976562, -0.968750, -0.960938,
-    -0.953125, -0.945312, -0.937500, -0.929688, -0.921875, -0.914062
-  };
-
-static const float reference_matrix_resnet50v1[FRAME_SIZE] = {
-  -123.680000, -115.779999, -101.940002, -120.680000, -112.779999, -98.940002,
-    -117.680000, -109.779999, -95.940002, -114.680000, -106.779999, -92.940002
-  };
-
-static const float reference_matrix_tinyyolov2[FRAME_SIZE] = {
-  0.000000, 0.003922, 0.007843, 0.011765, 0.015686, 0.019608,
-  0.023529, 0.027451, 0.031373, 0.035294, 0.039216, 0.043137
 };
 
 namespace mock {
@@ -122,7 +95,6 @@ class Frame : public r2i::IFrame {
 };
 
 class NormalizeMock : public r2i::Normalize {
-
  public:
   NormalizeMock () {
     this->dimensions.push_back(std::tuple<int, int>(FRAME_WIDTH, FRAME_HEIGHT));
@@ -140,35 +112,6 @@ class NormalizeMock : public r2i::Normalize {
   }
 
 };
-}
-
-/* Override constructors to use smaller matrices for testing */
-namespace r2i {
-
-NormalizeFaceNetV1::NormalizeFaceNetV1 () {
-  this->dimensions.push_back(std::tuple<int, int>(FRAME_WIDTH, FRAME_HEIGHT));
-};
-
-NormalizeInceptionV1::NormalizeInceptionV1 () {
-  this->dimensions.push_back(std::tuple<int, int>(FRAME_WIDTH, FRAME_HEIGHT));
-};
-
-NormalizeInceptionV3::NormalizeInceptionV3 () {
-  this->dimensions.push_back(std::tuple<int, int>(FRAME_WIDTH, FRAME_HEIGHT));
-};
-
-NormalizeResnet50V1::NormalizeResnet50V1 () {
-  this->dimensions.push_back(std::tuple<int, int>(FRAME_WIDTH, FRAME_HEIGHT));
-};
-
-NormalizeTinyyoloV2::NormalizeTinyyoloV2 () {
-  this->dimensions.push_back(std::tuple<int, int>(FRAME_WIDTH, FRAME_HEIGHT));
-};
-
-NormalizeTinyyoloV3::NormalizeTinyyoloV3 () {
-  this->dimensions.push_back(std::tuple<int, int>(FRAME_WIDTH, FRAME_HEIGHT));
-};
-
 }
 
 TEST_GROUP(Normalize) {
@@ -331,136 +274,6 @@ TEST(Normalize, NullOutputFrameData) {
 
   error = preprocessing->Apply(in_frame, out_frame);
   LONGS_EQUAL(r2i::RuntimeError::Code::NULL_PARAMETER, error.GetCode());
-}
-
-TEST_GROUP(NormalizeModels) {
-  r2i::RuntimeError error;
-  /* Preprocessing modules */
-  std::shared_ptr<r2i::IPreprocessing> preprocessing_facenetv1 =
-    std::make_shared<r2i::NormalizeFaceNetV1>();
-  std::shared_ptr<r2i::IPreprocessing> preprocessing_inceptionv1 =
-    std::make_shared<r2i::NormalizeInceptionV1>();
-  std::shared_ptr<r2i::IPreprocessing> preprocessing_inceptionv3 =
-    std::make_shared<r2i::NormalizeInceptionV3>();
-  std::shared_ptr<r2i::IPreprocessing> preprocessing_resnet50v1 =
-    std::make_shared<r2i::NormalizeResnet50V1>();
-  std::shared_ptr<r2i::IPreprocessing> preprocessing_tinyyolov2 =
-    std::make_shared<r2i::NormalizeTinyyoloV2>();
-  std::shared_ptr<r2i::IPreprocessing> preprocessing_tinyyolov3 =
-    std::make_shared<r2i::NormalizeTinyyoloV3>();
-
-  std::shared_ptr<unsigned char> dummy_frame_data;
-  std::shared_ptr<mock::Frame> in_frame = std::make_shared<mock::Frame>();
-
-  /* Output frames */
-  std::shared_ptr<mock::Frame> out_frame_facenetv1 =
-    std::make_shared<mock::Frame>();
-  std::shared_ptr<mock::Frame> out_frame_inceptionv1 =
-    std::make_shared<mock::Frame>();
-  std::shared_ptr<mock::Frame> out_frame_inceptionv3 =
-    std::make_shared<mock::Frame>();
-  std::shared_ptr<mock::Frame> out_frame_resnet50v1 =
-    std::make_shared<mock::Frame>();
-  std::shared_ptr<mock::Frame> out_frame_tinyyolov2 =
-    std::make_shared<mock::Frame>();
-  std::shared_ptr<mock::Frame> out_frame_tinyyolov3 =
-    std::make_shared<mock::Frame>();
-
-  void setup() {
-    error.Clean();
-
-    /* Fill frames with dummy data */
-    unsigned char value = 0;
-    dummy_frame_data = std::shared_ptr<unsigned char>(new unsigned char[FRAME_SIZE],
-                       std::default_delete<const unsigned char[]>());
-
-    unsigned char *in_data = dummy_frame_data.get();
-
-    for (unsigned int i = 0; i < FRAME_SIZE; i++) {
-      in_data[i] = value;
-      value++;
-    }
-
-    error = in_frame->Configure(in_data, FRAME_WIDTH, FRAME_HEIGHT,
-                                r2i::ImageFormat::Id::RGB);
-
-    std::shared_ptr<float> out_data_facenetv1 = std::shared_ptr<float>
-        (new float[FRAME_WIDTH * FRAME_HEIGHT * CHANNELS],
-         std::default_delete<float[]>());
-    std::shared_ptr<float> out_data_inceptionv1 = std::shared_ptr<float>
-        (new float[FRAME_WIDTH * FRAME_HEIGHT * CHANNELS],
-         std::default_delete<float[]>());
-    std::shared_ptr<float> out_data_inceptionv3 = std::shared_ptr<float>
-        (new float[FRAME_WIDTH * FRAME_HEIGHT * CHANNELS],
-         std::default_delete<float[]>());
-    std::shared_ptr<float> out_data_resnet50v1 = std::shared_ptr<float>
-        (new float[FRAME_WIDTH * FRAME_HEIGHT * CHANNELS],
-         std::default_delete<float[]>());
-    std::shared_ptr<float> out_data_tinyyolov2 = std::shared_ptr<float>
-        (new float[FRAME_WIDTH * FRAME_HEIGHT * CHANNELS],
-         std::default_delete<float[]>());
-    std::shared_ptr<float> out_data_tinyyolov3 = std::shared_ptr<float>
-        (new float[FRAME_WIDTH * FRAME_HEIGHT * CHANNELS],
-         std::default_delete<float[]>());
-    error = out_frame_facenetv1->Configure (out_data_facenetv1.get(), FRAME_WIDTH,
-                                            FRAME_HEIGHT,
-                                            r2i::ImageFormat::Id::RGB);
-    error = out_frame_inceptionv1->Configure (out_data_inceptionv1.get(),
-            FRAME_WIDTH,
-            FRAME_HEIGHT,
-            r2i::ImageFormat::Id::RGB);
-    error = out_frame_inceptionv3->Configure (out_data_inceptionv3.get(),
-            FRAME_WIDTH,
-            FRAME_HEIGHT,
-            r2i::ImageFormat::Id::RGB);
-    error = out_frame_resnet50v1->Configure (out_data_resnet50v1.get(), FRAME_WIDTH,
-            FRAME_HEIGHT,
-            r2i::ImageFormat::Id::RGB);
-    error = out_frame_tinyyolov2->Configure (out_data_tinyyolov2.get(), FRAME_WIDTH,
-            FRAME_HEIGHT,
-            r2i::ImageFormat::Id::RGB);
-    error = out_frame_tinyyolov3->Configure (out_data_tinyyolov3.get(), FRAME_WIDTH,
-            FRAME_HEIGHT,
-            r2i::ImageFormat::Id::RGB);
-
-  }
-};
-
-TEST(NormalizeModels, ApplySuccess) {
-  error = preprocessing_facenetv1->Apply(in_frame, out_frame_facenetv1);
-  LONGS_EQUAL(r2i::RuntimeError::Code::EOK, error.GetCode());
-
-  error = preprocessing_inceptionv1->Apply(in_frame, out_frame_inceptionv1);
-  LONGS_EQUAL(r2i::RuntimeError::Code::EOK, error.GetCode());
-
-  error = preprocessing_inceptionv3->Apply(in_frame, out_frame_inceptionv3);
-  LONGS_EQUAL(r2i::RuntimeError::Code::EOK, error.GetCode());
-
-  error = preprocessing_resnet50v1->Apply(in_frame, out_frame_resnet50v1);
-  LONGS_EQUAL(r2i::RuntimeError::Code::EOK, error.GetCode());
-
-  error = preprocessing_tinyyolov2->Apply(in_frame, out_frame_tinyyolov2);
-  LONGS_EQUAL(r2i::RuntimeError::Code::EOK, error.GetCode());
-
-  error = preprocessing_tinyyolov3->Apply(in_frame, out_frame_tinyyolov3);
-  LONGS_EQUAL(r2i::RuntimeError::Code::EOK, error.GetCode());
-
-  /* Check output values are the expected ones */
-  for (unsigned int i = 0; i < FRAME_SIZE; i++) {
-    DOUBLES_EQUAL( reference_matrix_facenetv1[i],
-                   static_cast<float *>(out_frame_facenetv1->GetData())[i], TOLERANCE);
-    DOUBLES_EQUAL( reference_matrix_inception[i],
-                   static_cast<float *>(out_frame_inceptionv1->GetData())[i], TOLERANCE);
-    DOUBLES_EQUAL( reference_matrix_inception[i],
-                   static_cast<float *>(out_frame_inceptionv3->GetData())[i], TOLERANCE);
-    DOUBLES_EQUAL( reference_matrix_resnet50v1[i],
-                   static_cast<float *>(out_frame_resnet50v1->GetData())[i], TOLERANCE);
-    DOUBLES_EQUAL( reference_matrix_tinyyolov2[i],
-                   static_cast<float *>(out_frame_tinyyolov2->GetData())[i], TOLERANCE);
-    DOUBLES_EQUAL( reference_matrix[i],
-                   static_cast<float *>(out_frame_tinyyolov3->GetData())[i], TOLERANCE);
-  }
-
 }
 
 int main(int ac, char **av) {
